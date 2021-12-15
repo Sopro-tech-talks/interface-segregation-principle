3b353f47bb4dd731377ab5a37a218e50d770d151
Vlatko Stankovikj <vlatko.stankovikj@sopro.io>
2021-12-15 11:44:59 +0100

    clean up

--- src/Shared/CocktailBase.cs
+++ src/Shared/CocktailBase.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Shared
 {
-    public abstract class CocktailBase
+    public abstract class CocktailBase : ICocktail<IIngredient, IngredientResponse>
     {
         protected List<IIngredient> Ingredients { get; set; }
         protected CocktailBase()
         {
 
             Ingredients = new List<IIngredient>();
+        }
+
+        public Task<IngredientResponse> AddIngredient(IIngredient ingredient)
+        {
+            Ingredients.Add(ingredient);
+            return Task.FromResult(new IngredientResponse { Name = $"Added {ingredient.Amount} of {ingredient.Name}" });
         }
     }
 }
